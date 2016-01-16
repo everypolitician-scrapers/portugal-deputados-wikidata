88421f58b051cc69d5675095cd0625eca3d86e21
Tony Bowden <tony@tmtm.com>
2016-01-16 10:21:58 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,41 +1,16 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'colorize'
 require 'pry'
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
 
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_de_Portugal',
+  after: '//span[@id="A"]',
+  xpath: '//table//td[1]//a[not(@class="new")]/@title',
+) 
 
-def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
-end
+EveryPolitician::Wikidata.scrape_wikidata(names: { pt: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
-def wikinames_from(url)
-  noko = noko_for(url)
-  noko.css('#A').xpath('preceding::*').remove
-  names = noko.xpath('//table//td[1]//a[not(@class="new")]/@title').map(&:text).uniq
-  raise "No names found in #{url}" if names.count.zero?
-  return names
-end
-
-def fetch_info(names)
-  WikiData.ids_from_pages('pt', names).each do |name, id|
-    data = WikiData::Fetcher.new(id: id).data('pt') rescue nil
-    unless data
-      warn "No data for #{p}"
-      next
-    end
-    data[:original_wikiname] = name
-    ScraperWiki.save_sqlite([:id], data)
-  end
-end
-
-fetch_info wikinames_from('https://pt.wikipedia.org/wiki/Lista_de_deputados_de_Portugal')
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
 
