ef75e6a243ae134819b5b187b1cb30fa0d8ac401
Tony Bowden <tony@tmtm.com>
2016-10-12 08:57:32 +0100

    Scrape German Wikipedia category as well
    We can get some extra people from https://de.wikipedia.org/wiki/Kategorie:Mitglied_der_Assembleia_da_Rep%C3%BAblica

--- scraper.rb
+++ scraper.rb
@@ -7,10 +7,11 @@ names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_de_Portugal',
   after: '//span[@id="A"]',
   xpath: '//table//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
-by_category = WikiData::Category.new( 'Categoria:Deputados da Assembleia da República Portuguesa', 'pt').member_titles
-  
-EveryPolitician::Wikidata.scrape_wikidata(names: { pt: names | by_category })
+by_category_pt = WikiData::Category.new( 'Categoria:Deputados da Assembleia da República Portuguesa', 'pt').member_titles
+by_category_de = WikiData::Category.new( 'Kategorie:Mitglied der Assembleia da República', 'de').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { pt: names | by_category_pt, de: by_category_de })
 
 
