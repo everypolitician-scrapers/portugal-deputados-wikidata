a9e0a21c5c24b51a28baf73c0322b67d81a44b53
Tony Bowden <tony@tmtm.com>
2017-05-08 08:58:03 +0100

    Also fetch via P39s

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_de_Portugal',
   after: '//span[@id="A"]',
   xpath: '//table//td[1]//a[not(@class="new")]/@title',
@@ -12,6 +12,21 @@ names = EveryPolitician::Wikidata.wikipedia_xpath(
 by_category_pt = WikiData::Category.new( 'Categoria:Deputados da Assembleia da República Portuguesa', 'pt').member_titles
 by_category_de = WikiData::Category.new( 'Kategorie:Mitglied der Assembleia da República', 'de').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { pt: names | by_category_pt, de: by_category_de })
+# Find all P39s of the 13th Legsislatura
+query = <<EOS
+  SELECT DISTINCT ?item
+  WHERE
+  {
+    BIND(wd:Q19953703 AS ?membership)
+    BIND(wd:Q25379987 AS ?term)
+
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 ?membership .
+    ?position_statement pq:P2937 ?term .
+  }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { pt: names | by_category_pt, de: by_category_de })
 
 
