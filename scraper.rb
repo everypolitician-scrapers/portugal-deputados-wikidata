8e07c2ae4d1c1775de6554a478affac2c7759b3b
Tony Bowden <tony@tmtm.com>
2019-02-06 15:08:28 +0000

    Remove other methods of finding members
    These are all subsets of the Wikidata members, other than including some
    people who we don't want (e.g. people who have only been MEPs, or who
    were members of earlier incarnations of the Assembly)

--- scraper.rb
+++ scraper.rb
@@ -3,15 +3,6 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://pt.wikipedia.org/wiki/Lista_de_deputados_de_Portugal',
-  after: '//span[@id="A"]',
-  xpath: '//table//td[1]//a[not(@class="new")]/@title',
-)
-
-by_category_pt = WikiData::Category.new( 'Categoria:Deputados da Assembleia da República Portuguesa', 'pt').member_titles
-by_category_de = WikiData::Category.new( 'Kategorie:Mitglied der Assembleia da República', 'de').member_titles
-
 # Find all P39s of the Legislature
 query = <<EOS
   SELECT DISTINCT ?item WHERE {
@@ -25,4 +16,4 @@ p39s = EveryPolitician::Wikidata.sparql(query)
 idquery = 'SELECT DISTINCT ?item WHERE { ?item wdt:P6199 [] }'
 ppids = EveryPolitician::Wikidata.sparql(idquery)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: p39s | ppids, names: { pt: names | by_category_pt, de: by_category_de })
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s | ppids)
